720c0a658a5ac92afda232c8c8f8f2da2341140d
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-07-03 12:09:07 +0700

    fix

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let speech = new SpeechSynthesisUtterance();
 
+let voices = [];
+
+let voiceSelect = document.getElementById("voiceSelect");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
+};
+
 document.getElementById("speakBtn").addEventListener("click", () => {
   speech.text = document.getElementById("textInput").value;
   window.speechSynthesis.speak(speech);
